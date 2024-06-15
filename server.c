8c0502385e3b9aa254c67c714e7f5039f159c6f8
stevenct84 <stevenct84@gmail.com>
2024-06-15 16:34:43 -0600

    server

---
+++ server.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+
+    // Crear el socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Asociar el socket al puerto
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("192.168.1.10"); // Especificar la IP del servidor
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Escuchar por conexiones entrantes
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Aceptar una conexión entrante
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Leer datos enviados por el cliente
+    read(new_socket, buffer, 1024);
+    printf("Message from client: %s\n", buffer);
+
+    // Enviar respuesta al cliente
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // Cerrar el socket
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
